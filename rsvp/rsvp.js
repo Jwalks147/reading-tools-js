6d913b78fe9730c1cbf3aab1b08f6df68327a146
JoeWalker <josephwalker61894@gmail.com>
2022-06-21 12:02:03 -0400

    added play and pause button

--- rsvp/rsvp.js
+++ rsvp/rsvp.js
@@ -1,10 +1,47 @@
 document.getElementById("reader-init").addEventListener("click", startRsvp);
 
+document.getElementById("reader-play")
+			.setAttribute("disabled", "true")
+		var stats = 0;
+
+function pauser() {
+    return new Promise(resolve => {
+        let playbuttonclick = function () {
+            document.getElementById("reader-pause")
+                .removeAttribute("disabled")
+
+            document.getElementById("reader-play")
+                .setAttribute("disabled", "true")
+
+            document.getElementById("reader-play")
+                .removeEventListener("click",
+                            playbuttonclick);
+
+            stats = 0;
+            resolve("resolved");
+        }
+        document.getElementById("reader-play")
+            .addEventListener("click", playbuttonclick)
+    })
+}
+
+ document.getElementById("reader-pause")
+			.addEventListener("click", function () {
+
+		stats = 1;
+
+			document.getElementById("reader-pause")
+				.setAttribute("disabled", "true")
+ 			document.getElementById("reader-play")
+				.removeAttribute("disabled")
+
+ 		})
+
 
 async function startRsvp() {
     let playing = true
-    document.getElementById("reader-play").onclick = () => playing = true;
     document.getElementById("reader-pause").onclick = () => playing = false;
+    document.getElementById("reader-play").onclick = () => playing = true;
 
     const readerDisplay = document.getElementById("reader-display");
     let WPM = document.getElementById("reader-wpm").value;
@@ -55,6 +92,7 @@ async function startRsvp() {
             // document.querySelector("#aria-test").innerText = word;
             await wait(basePeriod);
         }
+        if (stats == 1) await pauser();
     }
 
 }
